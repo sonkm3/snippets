44e1c58d999ca483e40219930c170aa0175e7a0c
Sousuke NAKAMURA <sonkm3@gmail.com>
2024-06-10 13:01:21 +0900

    wip

--- asyncwebserver.py
+++ asyncwebserver.py
@@ -1,26 +1,40 @@
 import asyncio
 
 
+# https://developer.mozilla.org/ja/docs/Web/HTTP/Messages
+
 class App:
     def __init__(self, host='0.0.0.0', port=80):
         self.host = host
         self.port = port
         self.handlers = []
 
-    def route(self, path, methods=['GET']):
-        def wrapper(handler):
-            self.handlers.append((path, methods, handler))
-            return handler
-        return wrapper
-
     async def dispatch(self, request, response):
-        line_list = []
+        request_line = (await request.readline()).decode('ascii').strip('\r\n')
+        method, path, version = request_line.split(' ')
+
+        print(f'method: {method}, path: {path}, version: {version}')
+
+        header_dict = {}
         while True:
-            line = await request.readline()
-            line = line.decode('ascii').strip('\r\n')
-            if line:
+            header_line = (await request.readline()).decode('ascii').strip('\r\n')
+            print(f'line: {header_line}')
+            if header_line == '':
                 break
-            line_list.append(line)
+            header_dict.update([header_line.split(': ', 1)])
+
+        charset = 'utf-8'
+        if 'Content-Type' in header_dict:
+            content_type = header_dict['Content-Type']
+            if 'charset=' in content_type:
+                charset = content_type.split('charset=')[1]
+
+        if 'Content-Length' in header_dict:
+            content_length = int(header_dict['Content-Length'])
+            body = (await request.read(content_length)).decode(charset)
+            print(f'body: {body}')
+
+        print(f'headers: {header_dict}')
 
         response.write(b'HTTP/1.0 404 Not Found\r\n\r\nNot Found')
         response.close()
@@ -29,7 +43,7 @@ class App:
     async def serve(self):
         await asyncio.start_server(self.dispatch, self.host, self.port)
 
-app = App(host='0.0.0.0', port=80)
+app = App(host='0.0.0.0', port=1080)
 def main():
     loop = asyncio.new_event_loop()
     # loop.set_debug(True)

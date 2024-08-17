d6f8e8f52591b0a014a7cc224918b60c4b478837
sonkm3 <sonkm3@gmail.com>
2024-08-17 20:54:39 +0900

    clean up

--- asyncwebserver.py
+++ asyncwebserver.py
@@ -5,10 +5,13 @@ import asyncio
 # https://developer.mozilla.org/ja/docs/Web/HTTP/Messages
 
 class WebServer:
-    def __init__(self, host='0.0.0.0', port=80):
+    def __init__(self, host='0.0.0.0', port=80, handlers={}):
         self.host = host
         self.port = port
-        self.handlers = []
+        self.handlers = handlers
+
+    def default_handler(self, method, path, request_header, query_dict={}, request_body=None):
+        pass
 
     async def read_request(self, request):
         return (await request.readline()).decode('ascii').strip('\r\n')
@@ -25,8 +28,17 @@ class WebServer:
         for header_line in header_list:
             key, value = self.parse_header_line(header_line)
             print(f'key: {key}, value: {value}')
-            header_dict[key] = value
+            header_dict.update({key: value})
         return header_dict
+
+    def parse_request_path(self, path):
+        query_dict = {}
+        if '?' in path:
+            path, query_string = path.split('?', 1)
+            for query_pair in query_string.split('&'):
+                key, value = query_pair.split('=', 1)
+                query_dict.update({key: value})
+        return path, query_dict
 
     async def read_header(self, request):
         header_list = []
@@ -44,6 +56,7 @@ class WebServer:
         headers = await self.read_header(request)
         method, path, version = self.parse_request_line(headers.pop(0))
         request_header = self.parse_request_header_to_dict(headers)
+        path, query_dict = self.parse_request_path(path)
 
         print(f'request_line {method} {path} {version}')
         print(f'request_header {request_header}')
@@ -51,6 +64,9 @@ class WebServer:
         if 'Content-Length' in request_header and int(request_header.get('Content-Length', 0)) > 0:
             request_body = await self.read_request_body(request, int(request_header.get('Content-Length', 0)))
             print(f'request_body {request_body}')
+
+        handler = self.handlers.get(path, self.default_handler)
+        response = handler(method, path, request_header, query_dict=query_dict, request_body=request_body)
 
         response.write(b'HTTP/1.0 404 Not Found\r\n\r\nNot Found')
         response.close()

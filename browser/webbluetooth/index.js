01353b5bf205b1f1b60c874bd1914221c7fddfb6
sonkm3 <sonkm3@gmail.com>
2025-07-18 16:29:52 +0900

    WIP

--- browser/webbluetooth/index.js
+++ browser/webbluetooth/index.js
@@ -4,8 +4,8 @@ let bluetooth = null;
 let state = null;
 
 const service_uuid = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
-const tx_charactaristics_uuid = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
-const rx_charactaristics_uuid = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
+const tx_charactaristics_uuid = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
+const rx_charactaristics_uuid = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
 
 document.addEventListener("DOMContentLoaded", (event) => {
     console.log("Initialize");
@@ -23,6 +23,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         service_uuid_element: document.getElementById("service_uuid"),
         tx_characteristics_element: document.getElementById("tx_characteristics"),
         rx_characteristics_element: document.getElementById("rx_characteristics"),
+        message_element: document.getElementById("message"),
     }
     state.service_uuid_element.value = service_uuid;
     state.tx_characteristics_element.value = tx_charactaristics_uuid;
@@ -32,7 +33,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
 async function choose_device() {
     console.log("choose_device");
     try{
-        state.target_device = await bluetooth.requestDevice({acceptAllDevices: true});
+        const requestDeviceOptionObj =             {
+                filters: [
+                    {services: [service_uuid.toLowerCase(),]},
+                    {namePrefix: "UART"}
+                ]
+            };
+
+        state.target_device = await bluetooth.requestDevice(requestDeviceOptionObj);
         console.log(state.target_device);
     } catch (error){
         console.log(error);
@@ -40,6 +48,20 @@ async function choose_device() {
         return;
     }
     state.device_name_element.value = state.target_device.name;
+
+    const gatt_server = await state.target_device.gatt.connect();
+    const uartService = await gatt_server.getPrimaryService(service_uuid.toLowerCase())
+    state.rxCharactaristics = await uartService.getCharacteristic(tx_charactaristics_uuid.toLowerCase());
+
+    state.rxCharactaristics.startNotifications();
+    state.rxCharactaristics.addEventListener("characteristicvaluechanged", handleRXUpdates)
+}
+
+async function handleRXUpdates(event){
+    if (event.type && event.type == "characteristicvaluechanged"){
+        const message = event.target.value.getUint8().toString();
+        state.message_element.value += "\r\n" + message;
+    }
 }
 
 async function send_to_device(){

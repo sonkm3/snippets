3d70199b1c0fd4440fdccb2176c088c60ee5f1f3
sonkm3 <sonkm3@gmail.com>
2025-09-03 16:24:07 +0900

    update type hint add api call function

--- aiortc/aiortc-whip/aiortc-whip.py
+++ aiortc/aiortc-whip/aiortc-whip.py
@@ -2,8 +2,10 @@
 import asyncio
 import platform
 from typing import Optional, Tuple
+from urllib.parse import urljoin
 
 import aiohttp
+from aiohttp.client import ClientResponse
 from aiortc import RTCConfiguration, RTCIceServer, RTCPeerConnection, RTCSessionDescription
 from aiortc.contrib.media import MediaPlayer
 
@@ -12,6 +14,29 @@ WHIP_SERVER_URL: str = 'http://localhost:8080/whip'
 ICE_SERVER_LIST = [RTCIceServer('stun:stun.l.google.com:19302'),]
 # ICE_SERVER_LIST = [RTCIceServer('stun:stun.l.google.com:19302'),RTCIceServer('stun:stun.cloudflare.com:19302'),]
 
+
+async def call_api(method: str, base_url: str, path: Optional[str] = '', headers: Optional[dict] = None, data: Optional[dict] = None) -> Optional[ClientResponse]:
+    if path and not base_url.endswith('/'):
+        url:str = urljoin(base_url + '/', path)
+    elif path:
+        url:str = urljoin(base_url, path)
+    else:
+        url:str = base_url
+
+    response: Optional[ClientResponse] = None
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.request(method, url, data=data, headers=headers) as resp:
+                if not resp.ok:
+                    print(f'response.status: {resp.status}')
+                else:
+                    await resp.read()
+                    response = resp
+        except aiohttp.ClientError as e:
+            print(e)
+            return None
+
+    return response
 
 def get_tracks() -> Tuple[MediaPlayer, Optional[MediaPlayer]]:
     video_options: dict = {'framerate': '30', 'video_size': '640x480'}
@@ -27,27 +52,22 @@ def get_tracks() -> Tuple[MediaPlayer, Optional[MediaPlayer]]:
             audio = None
     return webcam, audio
 
-async def send_offer(pc: RTCPeerConnection, url: str) -> str:
+async def send_offer(pc: RTCPeerConnection, url: str) -> Optional[str]:
     headers: dict = {'Content-Type': 'application/sdp'}
     local_sdp = pc.localDescription.sdp
 
-    async with aiohttp.ClientSession() as session:
-        try:
-            response = await session.post(url, data=local_sdp, headers=headers)
-        except aiohttp.ClientError as e:
-            print('aiohttp.ClientError')
-            return
-        if response.status not in (200, 201):
-            print('response.status')
-            return
+    response = await call_api('post', WHIP_SERVER_URL, data=local_sdp, headers=headers)
+    if response.status not in (200, 201):
+        print('response.status')
+        return
 
-        return await response.text()
+    return await response.text()
 
-async def apply_answer(pc: RTCPeerConnection, remote_sdp: str):
+async def apply_answer(pc: RTCPeerConnection, remote_sdp: str) -> None:
         await pc.setRemoteDescription(RTCSessionDescription(sdp=remote_sdp, type='answer'))
         return
 
-async def create_peer_connection():
+async def create_peer_connection() -> Optional[RTCPeerConnection]:
     pc: RTCPeerConnection = RTCPeerConnection(RTCConfiguration(ICE_SERVER_LIST))
 
     webcam: MediaPlayer
@@ -68,34 +88,49 @@ async def create_peer_connection():
     offer: RTCSessionDescription = await pc.createOffer()
     await pc.setLocalDescription(offer)
 
-    remote_sdp: str = await send_offer(pc, WHIP_SERVER_URL)
+    remote_sdp: Optional[str] = await send_offer(pc, WHIP_SERVER_URL)
     if not remote_sdp:
         return
     await apply_answer(pc, remote_sdp)
 
     return pc
 
-# pion WHIP-WHEPはDELETEに対応していないので使わない
-async def on_shutdown():
+# pionのwhip-whep exampleはDELETEに対応していないので使わない
+async def on_shutdown(pc: RTCPeerConnection) -> None:
     print('on_shutdown')
-    pass
-    # await pc.close()
-    async with aiohttp.ClientSession() as session:
-        response = await session.delete(WHIP_SERVER_URL)
-        print(f'DELETE request done with status: {response.status}')
 
-async def create_whip_connection():
-    # pcを明示的に止めたい
+    response = await call_api('post', WHIP_SERVER_URL)
+    print(response)
+
+    if pc:
+        await pc.close()
+
+
+async def create_whip_connection() -> Optional[RTCPeerConnection]:
     pc = await create_peer_connection()
+    return pc
 
 async def run():
     # try:
-    #     await asyncio.gather(create_whip_connection(), asyncio.Event().wait(),)
+    #     aiortc_main_task = asyncio.create_task(aiortc_whip_main())
     # except Exception as e:
-    #     print(f'Exception: {e}')
-    # finally:
-    #     await on_shutdown()
-    await asyncio.gather(create_whip_connection(), asyncio.Event().wait(),)
+    #     print(e)
+    #     aiortc_main_task.cancel()
+    #     await aiortc_main_task
+
+    pc = None
+    try:
+        pc = await create_whip_connection()
+    except Exception as e:
+        print(f'Exception: {e}')
+        await on_shutdown(pc)
+
+    try:
+        await asyncio.Event().wait()
+    except Exception as e:
+        print(f'Exception: {e}')
+    finally:
+        await on_shutdown(pc)
 
 if __name__ == '__main__':
     asyncio.run(run())

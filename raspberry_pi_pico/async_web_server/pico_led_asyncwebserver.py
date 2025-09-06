b5c0309a12d568847f7c4ccc53f530bffd9e8897
sonkm3 <sonkm3@gmail.com>
2025-09-07 00:00:25 +0900

    wip

---
+++ raspberry_pi_pico/async_web_server/pico_led_asyncwebserver.py
@@ -0,0 +1,58 @@
+import uasyncio as asyncio
+import machine
+import network
+import rp2
+import time
+
+from asyncwebserver import WebServer
+
+
+WIFI_COUNTRY = ''
+SSID = ''
+PSK = ''
+
+def main() -> None:
+    def setup_network() -> None:
+        network.hostname('micropython-demo')
+        rp2.country(WIFI_COUNTRY)
+
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+
+        wlan.connect(SSID, PSK)
+
+        while not (wlan.isconnected() and wlan.status() == network.STAT_GOT_IP):
+            print("Waiting to connect:")
+            time.sleep(1)
+
+        print(wlan.ifconfig())
+
+    def setup_handlers() -> dict:
+        led = machine.Pin('LED', machine.Pin.OUT)
+        def led_on():
+            led.value(1)
+        
+        def led_off():
+            led.value(0)
+
+        def led_on_handler(method, path, request_header, query_dict={}, request_body=None):
+            led_on()
+            return b'HTTP/1.0 200 OK\r\n\r\nLED ON'
+
+        def led_off_handler(method, path, request_header, query_dict={}, request_body=None):
+            led_off()
+            return b'HTTP/1.0 200 OK\r\n\r\nLED OFF'
+
+        return {'/led_on': led_on_handler, '/led_off': led_off_handler}
+
+    setup_network()
+    handlers = setup_handlers()
+
+    web_server = WebServer(host='0.0.0.0', port=2080, handlers=handlers)
+
+    loop = asyncio.new_event_loop()
+    loop.create_task(web_server.serve())
+    loop.run_forever()
+
+if __name__ == '__main__':
+    main()
